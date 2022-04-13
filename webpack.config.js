4cf7c9da1285f752dcee72c3be721419f606eb6b
Wing Chau <wing@devtography.com>
2022-04-14 03:42:01 +0800

    Fix Webpack DevServer config

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,9 @@ webAppModule.module.rules.push({
   use: ['style-loader', 'css-loader', 'sass-loader'],
 });
 webAppModule.devServer = {
-  contentBase: path.join(__dirname, 'public'),
+  static: {
+    directory: path.join(__dirname, 'public'),
+  },
   port: 3000,
   hot: true,
   historyApiFallback: true,
